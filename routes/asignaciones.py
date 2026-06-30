89ee5e50a580034a9ed5483a11501e614dca3c47
AlexRgl21 <brayanrgl070806@gmail.com>
2026-06-30 15:59:05 -0600

    feat(backend): implementar ruta para registro de grupos en BD
    - Se creó el endpoint POST '/asignaciones/agregar_grupo' en el controlador de asignaciones.
    - Se implementó la lógica de extracción de datos del formulario (nombre, generaciones y carrera).
    - Se añadió manejo de datos nulos para permitir que 'generacion_fin' quede en blanco sin romper la base de datos.
    - Se integró la consulta SQL (INSERT) respetando el esquema, las llaves foráneas y el estatus por defecto.
    - Se configuró el flujo para redirigir automáticamente a la vista de asignaciones tras completar el registro.

--- routes/asignaciones.py
+++ routes/asignaciones.py
@@ -1,7 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from database.conexion import get_db_connection
 
 asignaciones_bp = Blueprint('asignaciones', __name__)
 
 @asignaciones_bp.route('/asignaciones')
 def vista_asignaciones():
     return render_template('asignaciones.html')
+
+@asignaciones_bp.route('/asignaciones/agregar_grupo', methods=['POST'])
+def agregar_grupo():
+    nombre_grupo = request.form('nombre_grupo')
+    generacion_inicio = request.form('generacion_inicio')
+    generacion_fin = request.form('generacion_fin')
+    id_carrera = request.form('id_carrera')
+
+    if not generacion_fin or generacion_fin.strip() == "":
+        generacion_fin = None
+    else:
+        generacion_fin = int(generacion_fin)
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    try:
+        cursor.execute("""
+            INSERT INTO Grupos (nombre_grupo, generacion_inicio, generacion_fin, id_carrera, estatus)
+            VALUES (?, ?, ?, ?, 'Activo') 
+        """, (nombre_grupo, int(generacion_inicio), generacion_fin, int(id_carrera) ))
+
+        conn.commit()
+    except Exception as e:
+        print(f"Error al guardar el grupo {e}")
+        conn.rollback()
+    finally:
+        conn.close()
+
+    return redirect(url_for('asignaciones.vista_asignaciones'))
